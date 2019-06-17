f082f3115b6d10762bdb66163f538ccfed6e33bf
Leszek Jakubowski <ljakubow@redhat.com>
2019-06-17 14:46:42 +0200

    added docs, reformatted python

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # prometheus-example
-An example python app with simple prometheus metrics
+An example python app with simple prometheus metrics. It's intended as a simple application for testing Prometheus scraping inside Openshift clusters.
+
+It will respond with prometheus metrics on all paths over HTTP on port 8080.
+
+# Credits
+This is the example from https://github.com/prometheus/client_python, reformatted and ready for instant deployment.
+
+# Usage
+To deploy inside an Openshift cluster run
+```
+oc new-app python:3.6~https://github.com/Makdaam/prometheus-example
+```
+
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 from prometheus_client import start_http_server, Summary
 import random
 import time
 
 # Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+REQUEST_TIME = Summary("request_processing_seconds", "Time spent processing request")
 
 # Decorate function with metric.
 @REQUEST_TIME.time()
@@ -11,10 +12,10 @@ def process_request(t):
     """A dummy function that takes some time."""
     time.sleep(t)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Start up the server to expose the metrics.
     start_http_server(8080)
     # Generate some requests.
     while True:
         process_request(random.random())
-
